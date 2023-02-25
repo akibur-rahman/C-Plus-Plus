e2e648953b5ce8239ee11dd424e7409a1d356495
Akibur Rahman <me.akiburrahman@gmail.com>
2023-02-25 14:24:40 +0600

    maps updated

--- STL_Maps.cpp
+++ STL_Maps.cpp
@@ -3,7 +3,16 @@ using namespace std;
 
 void printMap(map<int, string> &m)
 {
-    cout << m.size() << endl;
+    cout << "Size: " << m.size() << endl;
+    for (auto value : m)
+    {
+        cout << value.first << " " << value.second << endl;
+    }
+}
+
+void printUMap(unordered_map<int, string> &m)
+{
+    cout << "Size: " << m.size() << endl;
     for (auto value : m)
     {
         cout << value.first << " " << value.second << endl;
@@ -18,10 +27,22 @@ int main(int argc, char const *argv[])
     m[2] = "test";
     m[3] = "world";
     // we can use find method to find value of a key. it returns an itreator
-    auto it = m.find(3);
+    auto it = m.find(3); // finds the value of key inserted
     cout << it->first << " " << it->second;
+    cout << endl;
 
     printMap(m);
+
+    m.erase(3); // removes the key value of pair of provided key
+
+    printMap(m);
+
+    // Maps can be unordered as well. it doesnt sort the keys. Time complexity: O(1)
+    unordered_map<int, string> um;
+    um[3] = "hello";
+    um[1] = "test";
+    um[2] = "world";
+    printUMap(um);
 
     return 0;
 }
