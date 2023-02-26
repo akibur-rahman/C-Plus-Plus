c68f9ca7ddda052c4742af3583e4bef8395805f6
Akibur Rahman <me.akiburrahman@gmail.com>
2023-02-26 21:56:24 +0600

    Update STL_Iterator.cpp

--- STL_Iterator.cpp
+++ STL_Iterator.cpp
@@ -6,7 +6,7 @@ int main(int argc, char const *argv[])
 
     // iterators are like pointer taht points at memory address of containers
     vector<int> v = {1, 2, 3, 4, 5, 6, 7};
-    vector<int>::iterator it = v.begin();
+    vector<int>::iterator it = v.begin(); //.begin() points at the first of element of the container and .end() points at next to last element of the container
     for (it = v.begin(); it != v.end(); it++)
     {
         cout << (*it) << endl;
